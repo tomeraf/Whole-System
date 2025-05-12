dd441bc4f33e9e3b25e4fe8f3bbb42904b9425f9
Ben_Rubinovitz <benru@post.bgu.ac.il>
2025-05-12 12:11:17 +0300

    fixed beans error but another one came up unfortunately.

--- workshop-project/src/main/java/com/halilovindustries/frontend/application/presenters/HomePresenter.java
+++ workshop-project/src/main/java/com/halilovindustries/frontend/application/presenters/HomePresenter.java
@@ -4,7 +4,7 @@ import com.halilovindustries.backend.Domain.DTOs.ItemDTO;
 import com.halilovindustries.backend.Domain.DTOs.ShopDTO;
 import com.halilovindustries.backend.Service.ShopService;
 import com.halilovindustries.backend.Service.UserService;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -13,11 +13,11 @@ import java.util.Random;
 
 @Component
 public class HomePresenter {
-    UserService userService;
-    ShopService shopService;
-    List<ShopDTO> RandomShops;
+    private final UserService userService;
+    private final ShopService shopService;
+    private List<ShopDTO> randomShops = new ArrayList<>();
 
-
+    @Autowired
     public HomePresenter(UserService userService, ShopService shopService) {
         this.userService = userService;
         this.shopService = shopService;
@@ -25,32 +25,55 @@ public class HomePresenter {
     }
 
     private void rnd3Shops() {
+//        List<ShopDTO> shops = shopService.showAllShops().getData();
+//        Random rand = new Random();
+//        if (shops.isEmpty())
+//            return;
+//        int rndNum = rand.nextInt(shops.size());
+//        RandomShops = new ArrayList<>();
+//        for (int i = 0; i < 3; i++)
+//            RandomShops.add(shops.get(rndNum++ % shops.size()));
+
         List<ShopDTO> shops = shopService.showAllShops().getData();
+        randomShops.clear();
+        if (shops == null || shops.isEmpty()) return;
+
         Random rand = new Random();
-        if (shops.isEmpty())
-            return;
-        int rndNum = rand.nextInt(shops.size());
-        RandomShops = new ArrayList<>();
-        for (int i = 0; i < 3; i++)
-            RandomShops.add(shops.get(rndNum++ % shops.size()));
+        int start = rand.nextInt(shops.size());
+        for (int i = 0; i < 3; i++) {
+            randomShops.add(shops.get((start + i) % shops.size()));
+        }
     }
 
-    @Bean
+
     public List<ShopDTO> getRandomShops() {
-        return RandomShops;
+        return randomShops;
     }
 
-    @Bean
+
     public List<ItemDTO> get4rndShopItems(ShopDTO shop) {
-        List<ItemDTO> RandomItems = new ArrayList<>();
-        Random rand = new Random();
-        if (RandomShops.isEmpty())
-            return RandomItems;
+//        List<ItemDTO> randomItems = new ArrayList<>();
+//        Random rand = new Random();
+//        if (randomShops.isEmpty())
+//            return randomItems;
+//        Object[] keys = shop.getItems().keySet().toArray();
+//        int rndNum = rand.nextInt(keys.length);
+//        for (int i = 0; i < 4; i++)
+//            randomItems.add(shop.getItems().get((Integer)(keys[rndNum%keys.length])));
+//        return randomItems;
+
+        List<ItemDTO> randomItems = new ArrayList<>();
+        if (shop.getItems() == null || shop.getItems().isEmpty())
+            return randomItems;
+
         Object[] keys = shop.getItems().keySet().toArray();
-        int rndNum = rand.nextInt(keys.length);
-        for (int i = 0; i < 4; i++)
-            RandomItems.add(shop.getItems().get((Integer)(keys[rndNum%keys.length])));
-        return RandomItems;
+        Random rand = new Random();
+        int start = rand.nextInt(keys.length);
+        for (int i = 0; i < 4; i++) {
+            int idx = (start + i) % keys.length;
+            randomItems.add(shop.getItems().get((Integer)keys[idx]));
+        }
+        return randomItems;
     }
 
 
